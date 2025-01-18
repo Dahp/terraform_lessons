5883fffb48072610582b9c8b234929d747ac41f2
Daniil Miranovich <pr0p0wer00123@gmail.com>
2025-01-18 20:05:49 +0300

    Init

--- Lesson_1/main.tf
+++ Lesson_1/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "name1" {
-  count         = 3                       # Создаст 3 сервера
+  count         = 3                       # Создаст 3 instance
   ami           = "ami-04b4f1a9cf54c11d0" # надо брать только для того региона, в котором поднимается instance. Для каждого региона свои ami для тех же самых образов
   instance_type = "t3.micro"
   tags = {
