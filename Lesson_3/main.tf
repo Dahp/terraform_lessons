ee0a9655d5cdf6be0408a8c8828def615b4e5a98
Daniil Miranovich <pr0p0wer00123@gmail.com>
2025-01-19 13:26:50 +0300

    +

--- Lesson_3/main.tf
+++ Lesson_3/main.tf
@@ -1,90 +1,21 @@
+#=================== provider
 provider "aws" {
-  region = "us-east-1"
-  default_tags { # тэги которые будут применениы к каждому ресурсу
+  #region = "us-east-1" #теперь берется из .env
+  default_tags {
     tags = {
-      "Owner"      = "Daniil"
-      "Enviroment" = "Lesson 2"
+      "Owner"  = "Daniil"
+      "Number" = "Lesson_3"
     }
   }
 }
 
-#================================== aws_instance
-resource "aws_instance" "Server" {
-  #count         = 1                       # даже если тут один, то это все равно будет массив и обращаться надо будет через []
-  ami           = "ami-043a5a82b6cf98947" # ami - amazom machine images
+#=================== instance
+
+resource "aws_instance" "AWS-Linux" {
+  count         = 1 # тут все равно будет массив и надо будет обращаться через [0]
+  ami           = "ami-043a5a82b6cf98947"
   instance_type = "t2.nano"
   tags = {
-    "Name" = "Linux AWS"
-  }
-  vpc_security_group_ids = [aws_security_group.SG_port_22.id, aws_security_group.SG_ports_80_443.id]
-  # важно в конце aws_security_group.SG_port_22 указывать.id, так как тут нуженн массив ids
-  # когда мы привяваем SG через ресурс(aws_security_group), то автоматически создается зависимость для SG, 
-  # те сначала создатся instance, потом к нему прикрепятся SG
-
-  user_data = <<EOF
-#!/bin/bash
-apt update -y
-apt upgrade -y
-  EOF
-  # это использование user_data, те то что мы сюда передаем, будет выволненно от имени root как скрипт
-}
-
-#================================== aws_security_group
-# 1 ingress\engree = 1 правило
-# те таких ingress\engree в одном resource можно создавать сколько угодно
-
-resource "aws_security_group" "SG_port_22" {
-  name        = "SG_port_22"
-  description = "1-Allow ssh connection "
-
-  ingress { # приходит на инстансе
-    from_port        = 22
-    to_port          = 22
-    protocol         = "TCP" # если указываем порт, то нужно указывать протокол
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {                  # уходит с инстансе
-    from_port        = 0    # любой
-    to_port          = 0    # любой 
-    protocol         = "-1" # любой 
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  tags = {
-    "Name" = "SG Linux AWS"
-  }
-}
-
-resource "aws_security_group" "SG_ports_80_443" {
-  name        = "SG_ports_80_443"
-  description = "1-Allow ssh connection "
-
-  ingress { # правило для входящего трафика по 80 порту
-    from_port        = 80
-    to_port          = 80
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress { # правило для входящего трафика по 443 порту
-    from_port        = 443
-    to_port          = 443
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {                  # уходит с инстансе
-    from_port        = 0    # любой
-    to_port          = 0    # любой 
-    protocol         = "-1" # любой
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  tags = {
-    "Name" = "SG Linux AWS"
+    "Name" = "Back-end"
   }
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,16 @@
 Импорт переменных среды `export $(cat .env | xargs)`
+Внутри файла `.env`
+```ruby
+AWS_DEFAULT_REGION=us-east-1
+AWS_ACCESS_KEY_ID=""
+AWS_SECRET_ACCESS_KEY=""
+```
 ---
 1) **Lesson_1**: aws_instance, default_tags
 2) **Lesson_2**: 
     - Создаем инстансе aws_instance
     - Создаем SG aws_security_group, с несколькими rule ingress/engress
-    - Подключаем (attche) созданные SG к aws_security_group при помощи vpc_security_group_id = []
+    - Подключаем (attache) созданные SG к aws_security_group при помощи vpc_security_group_id = []
+    - Используем user_data = file("file.sh") для выполнения скрипта при инициализации
+3) **Lesson_3**:
+    - 
